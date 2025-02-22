2a4becb2471fc5a8d0abea7a322d8d7d8803d8ee
Fluke Sikharin <sikharin@kkumail.com>
2025-02-23 00:06:40 +0700

    2nd commit

--- netfluke-frontend/app/services/movieService.ts
+++ netfluke-frontend/app/services/movieService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000';
+const BASE_URL = process.env.NEST_BACKEND_URL || 'http://localhost:4000';
 
 export const fetchHomeMovies = async () => {
     try {
