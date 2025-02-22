d0d3952403f07f5882f173ff6885b21b51855f3f
Fluke Sikharin <sikharin@kkumail.com>
2025-02-23 01:09:28 +0700

    commit

--- netfluke-frontend/app/components/ProfileList.tsx
+++ netfluke-frontend/app/components/ProfileList.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Image from "next/image";
-import profileImg from "@/public/images/fluke.jpg";
 import { FaChildReaching } from "react-icons/fa6";
 
 interface ProfileListProps {
